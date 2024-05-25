a5d095a0ea98323d9723bc9fc47627bff683f1d0
GlitchO0 <100947381+GlitchO0@users.noreply.github.com>
2024-05-25 14:23:17 +0300

    Add files via upload

---
+++ specch2txtApp.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import speech_recognition as sr
+
+app = Flask(__name__)
+
+def recognize_speech():
+    r = sr.Recognizer()
+    response = ""
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        response += "Please say something...\n"
+
+        try:
+            audio = r.listen(source)
+            recognized_text = r.recognize_google(audio)
+            response += "You have said: \n" + recognized_text
+        except sr.UnknownValueError:
+            response += "The Speech Recognition model didn't understand the audio"
+        except Exception as e:
+            response += "Error: " + str(e)
+
+    return response
+
+@app.route('/recognize_speech', methods=['GET'])
+def recognize_speech_route():
+    result = recognize_speech()
+    return result
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
